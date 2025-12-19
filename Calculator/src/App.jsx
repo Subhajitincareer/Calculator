c5f92c3f99652219850b3046454a03c9536f2313
w3schools-test <subhajitincareer@gmail.com>
2025-12-19 19:55:17 +0530

    update 1.0.0

--- Calculator/src/App.jsx
+++ Calculator/src/App.jsx
@@ -1,21 +1,12 @@
 import React from "react";
 import image from "./assets/Calculator.jpg";
+import Calculator from "./Components/Calculator.jsx";
+
 const App = () => {
   return (
-    <div className="w-screen h-screen flex justify-center items-center bg-gray-200 small-mobile:w-screen small-mobile:h-screen small-mobile:bg-red-500">
-      <div className="w-96 h-96 bg-amber-200 grid grid-cols-4">
-        <div className="h-full bg-amber-700 flex flex-col items-center gap-10">
-          <div className="text-3GITHUBxl">2</div>
-          <div className="text-3xl">3</div>
-          <div className="text-3xl">4</div>
-          <div className="text-3xl">5</div>
-          <div className="text-3xl">1</div>
-        </div>
-        <div></div>
-        <div></div>
-        <div></div>
-      </div>
-    </div>
+    <>
+      <Calculator />
+    </>
   );
 };
 
