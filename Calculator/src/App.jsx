9c8f3fa76f4ec54d1523dab04823b437ef2e458f
w3schools-test <subhajitincareer@gmail.com>
2025-12-19 18:01:55 +0530

    EXPERIMENT DONE FOR LAYOUT

--- Calculator/src/App.jsx
+++ Calculator/src/App.jsx
@@ -2,8 +2,19 @@ import React from "react";
 import image from "./assets/Calculator.jpg";
 const App = () => {
   return (
-    <div className="w-screen h-screen">
-      <img src={image} alt="Calculator" className="w-full h-full" />
+    <div className="w-screen h-screen flex justify-center items-center bg-gray-200">
+      <div className="w-96 h-96 bg-amber-200 grid grid-cols-4">
+        <div className="h-full bg-amber-700 flex flex-col items-center gap-10">
+          <div className="text-3GITHUBxl">2</div>
+          <div className="text-3xl">3</div>
+          <div className="text-3xl">4</div>
+          <div className="text-3xl">5</div>
+          <div className="text-3xl">1</div>
+        </div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
   );
 };
